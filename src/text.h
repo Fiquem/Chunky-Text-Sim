915b12e168d167f836cd3543ab8e6d42fd200f60
Emma Carrigan <emmeridge@hotmail.com>
2016-08-08 19:13:45 +0100

    urgh I am struggling so hard trying to figure out fonts

--- src/text.h
+++ src/text.h
@@ -1,14 +1,18 @@
+#pragma once
 
+#include "gl_utils.h"
 
-typedef struct Text{
-	// uhhhhhhhhhhhhhhhhh
-}Text;
+typedef struct Character{
+	float xpos, ypos;
+	float width, height;
+	float buffer;
+}Character;
 
-// do I need font to be a different thing?
 typedef struct Font{
 	// uhhhhhhhhhhhhhhhhhjjhhhhhhhhhhhhh
 	GLuint texture;
 	GLuint shader;
+	int size;
 }Font;
 
 Font load_font(const char* font_img, const char* font_meta);
